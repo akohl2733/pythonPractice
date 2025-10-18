6ac1d0eb5f2e994a851c038e8cffb9bff105cee8
Andrew <akohl2733@gmail.com>
2025-10-18 00:50:18 -0400

    got database set up and opened up the post and get endpoints for /users

--- pythonWeek7/small-practice-projects/fastapi/main.py
+++ pythonWeek7/small-practice-projects/fastapi/main.py
@@ -1,25 +1,62 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from typing import Optional, List
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
+import sys, os
+
 
 app = FastAPI()
+
+
+# SQLite DB setup
+class Base(DeclarativeBase):
+    pass
+
+DATABASE_URL = ("sqlite:///./test.db")
+engine = create_engine(DATABASE_URL, echo=True)
+
+class User(Base):
+    __tablename__ = "user_info"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    age: Mapped[int]
+    company: Mapped[str]
+
+Base.metadata.create_all(bind=engine)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
 
 class UserModel(BaseModel):
     name: str
     age: int
     company: str
 
-user_list: List[UserModel] = [] 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 async def root():
     return { 'Hello': "World" }
 
+
 @app.post("/users")
-def post_user(user: UserModel):
-    user_list.append(user)
+def post_user(user: UserModel, db: SessionLocal = Depends(get_db)):
+    user_data = user.model_dump()
+    db_user = User(**user_data)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
     return { "message": "User Created", "user": user}
 
+
 @app.get("/users")
-async def get_users() -> List[UserModel]:
-    return user_list
+async def get_users(db: SessionLocal = Depends(get_db)) -> List[UserModel]:
+    users = db.query(User).all()
+    return users
